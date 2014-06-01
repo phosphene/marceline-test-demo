e3fb90ae8099d31be2cd4cd30df84e4c9f4652e9
phosphene <ed@cronos.net>
2014-06-01 15:21:42 -0400

    add marceline and storm to project.clj

--- project.clj
+++ project.clj
@@ -1,9 +1,18 @@
 (defproject marceline-test-demo "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Marceline Demonstration"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]]
   :main ^:skip-aot marceline-test-demo.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles { 
+             :dev
+             {:dependencies [
+                             [storm "0.9.0.1"]
+                             [yieldbot/marceline "0.2.0-SNAPSHOT"]
+                             [midje "1.6.0"]
+                             [lein-midje "3.1.3"]]}
+
+
+:uberjar {:aot :all}})
