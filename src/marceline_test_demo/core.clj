c32ab5bfce7bee145fe3ad12ff3b238e2397377c
phosphene <ed@cronos.net>
2014-06-01 17:23:16 -0400

    first passing test of word count example

--- src/marceline_test_demo/core.clj
+++ src/marceline_test_demo/core.clj
@@ -1,5 +1,5 @@
 (ns marceline-test-demo.core
- (:import storm.trident.TridentTopology
+  (:import storm.trident.TridentTopology
            [storm.trident.operation.builtin MapGet]
            [storm.trident.testing MemoryMapState$Factory FixedBatchSpout]
            [backtype.storm LocalDRPC LocalCluster StormSubmitter])
@@ -8,7 +8,70 @@
   (:use [backtype.storm config])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn mk-fixed-batch-spout [max-batch-size]
+  (FixedBatchSpout.
+   (t/fields "sentence")
+   max-batch-size
+   (into-array (map t/values '("the cow jumped over the moon"
+                               "the man went to the store and bought some candy"
+                               "four score and seven years ago"
+                               "how many can you eat"
+                               "to be or not to be the person")))))
+
+(t/defcombineraggregator
+  count-words
+  ([] 0)
+  ([tuple] 1)
+  ([t1 t2] (+ t1 t2)))
+
+(t/defcombineraggregator
+  sum
+  ([] 0)
+  ([tuple] (t/get tuple :count)) ;; fields can be specified with keywords or strings
+  ([t1 t2] (+ t1 t2)))
+
+(t/deftridentfn
+  split-args
+  [tuple coll]
+  (when-let [args (t/first tuple)]
+    (let [words (string/split args #" ")]
+      (doseq [word words]
+        (t/emit-fn coll word)))))
+
+(t/deffilter
+  filter-null
+  [tuple]
+  (not (nil? (t/first tuple))))
+
+(defn build-topology [spout drpc]
+  (let [word-state-factory (MemoryMapState$Factory.)
+        trident-topology (TridentTopology.)
+        word-counts (-> (t/new-stream trident-topology "word-counts" spout)
+                        (t/parallelism-hint 16)
+                        (t/each ["sentence"]
+                                split-args
+                                ["word"])
+                        (t/project ["word"])
+                        (t/group-by ["word"])
+                        (t/persistent-aggregate word-state-factory
+                                                ["word"]
+                                                count-words
+                                                ["count"])
+                        (t/parallelism-hint 16))
+        count-query (-> (t/drpc-stream trident-topology "words" drpc)
+                        (t/each ["args"]
+                                split-args
+                                ["word"])
+                        (t/project ["word"])
+                        (t/group-by ["word"])
+                        (t/state-query word-counts
+                                       ["word"]
+                                       (MapGet.)
+                                       ["count"])
+                        (t/each ["count"] filter-null)
+                        (t/aggregate ["count"]
+                                     sum
+                                     ["sum"])
+                        (t/debug))]
+    trident-topology))
+
--- test/marceline_test_demo/core_test.clj
+++ test/marceline_test_demo/core_test.clj
@@ -1,7 +1,73 @@
 (ns marceline-test-demo.core-test
+  (:import storm.trident.TridentTopology
+           [storm.trident.operation.builtin MapGet]
+           [storm.trident.testing MemoryMapState$Factory FixedBatchSpout]
+           [backtype.storm LocalDRPC LocalCluster StormSubmitter])
   (:require [clojure.test :refer :all]
-            [marceline-test-demo.core :refer :all]))
+            [marceline-test-demo.core :refer :all]
+            [backtype.storm.testing :as t])
+  (:use clojure.test
+        storm.trident.testing
+        marceline-test-demo.core
+        [backtype.storm config]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(defn run-local! []
+  (let [cluster (LocalCluster.)
+        local-drpc (LocalDRPC.)
+        spout (doto (mk-fixed-batch-spout 3)
+                (.setCycle true))]
+    (.submitTopology cluster "wordcounter"
+                     {}
+                     (.build
+                      (build-topology
+                       spout
+                       local-drpc)))
+    (Thread/sleep 10000)
+    (.execute local-drpc "words" "cat dog the man jumped")
+    (.shutdown cluster)
+    (System/exit 0)))
+
+(defn submit-topology! [env]
+  (let [name "wordcounter"
+        conf {TOPOLOGY-WORKERS 6
+              TOPOLOGY-MAX-SPOUT-PENDING 20
+              TOPOLOGY-MESSAGE-TIMEOUT-SECS 60
+              TOPOLOGY-STATS-SAMPLE-RATE 1.00}
+        spout (doto (mk-fixed-batch-spout 3)
+                (.setCycle true))]
+    (StormSubmitter/submitTopology
+     name
+     conf
+     (.build (build-topology
+              spout
+              nil)))))
+
+(defn -main
+  ([]
+     (run-local!))
+  ([env & args]
+     (submit-topology! (keyword env))))
+
+
+(def TEST-VALS [["the cow jumped over the moon"]
+                ["four score and seven years ago"]
+                ["how many can you eat"]
+                ["to be or not to be the person"]])
+
+
+(deftest wordcount-drpc
+  (t/with-local-cluster [cluster]
+    (with-drpc [drpc]
+      (let [feeder (feeder-spout ["sentence"])
+            topology (build-topology feeder drpc)]
+        (with-topology [cluster topology]
+          (feed feeder TEST-VALS)
+          (is (= 4
+                 (ffirst
+                  (exec-drpc drpc
+                             "words"
+                             "cat dog the man jumped")))))))))
+
+
+
