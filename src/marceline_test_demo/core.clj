7d8a891b9d4f3ae5dafc2106baf9600fb84ec21c
phosphene <ed@cronos.net>
2014-06-01 15:22:51 -0400

    add compiling import and require statements to core.clj

--- src/marceline_test_demo/core.clj
+++ src/marceline_test_demo/core.clj
@@ -1,4 +1,11 @@
 (ns marceline-test-demo.core
+ (:import storm.trident.TridentTopology
+           [storm.trident.operation.builtin MapGet]
+           [storm.trident.testing MemoryMapState$Factory FixedBatchSpout]
+           [backtype.storm LocalDRPC LocalCluster StormSubmitter])
+  (:require [marceline.storm.trident :as t]
+            [clojure.string :as string :only [split]])
+  (:use [backtype.storm config])
   (:gen-class))
 
 (defn -main
